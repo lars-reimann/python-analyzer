086898183c6aa63c306e6d24a51fbd9c8f1316ef
Lars Reimann <mail@larsreimann.com>
2021-08-11 10:50:34 +0200

    refactor: extract CLI part

---
+++ library_analyzer/cli.py
@@ -0,0 +1,41 @@
+import argparse
+from pathlib import Path
+
+from library_analyzer.call_and_parameter_counter import count_calls_and_parameters
+
+
+def cli() -> None:
+    args = __get_args()
+
+    if args.command == "count":
+        count_calls_and_parameters(args.src, args.exclude, args.out)
+
+
+def __get_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Analyze Python code.")
+    subparsers = parser.add_subparsers(dest="command")
+
+    # count command
+    count_parser = subparsers.add_parser(
+        "count",
+        help="Count how often functions are called/parameters are used and with which values."
+    )
+    count_parser.add_argument(
+        "-s",
+        "--src",
+        help="Directory containing Python code.",
+        type=Path,
+        required=True,
+    )
+    count_parser.add_argument(
+        "-e",
+        "--exclude",
+        help="File with list of file names to exclude. Gets updated as Python source files are processed.",
+        type=Path,
+        required=True,
+    )
+    count_parser.add_argument(
+        "-o", "--out", help="Output file.", type=Path, required=True
+    )
+
+    return parser.parse_args()
--- library_analyzer/main.py
+++ library_analyzer/main.py
@@ -1,47 +1,12 @@
-import argparse
 import time
-from pathlib import Path
 
-from .call_and_parameter_counter import count_calls_and_parameters
-
-
-def get_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description="Analyze Python code.")
-    subparsers = parser.add_subparsers(dest="command")
-
-    # count command
-    count_parser = subparsers.add_parser(
-        "count",
-        help="Count how often functions are called/parameters are used and with which values."
-    )
-    count_parser.add_argument(
-        "-s",
-        "--src",
-        help="Directory containing Python code.",
-        type=Path,
-        required=True,
-    )
-    count_parser.add_argument(
-        "-e",
-        "--exclude",
-        help="File with list of file names to exclude. Gets updated as Python source files are processed.",
-        type=Path,
-        required=True,
-    )
-    count_parser.add_argument(
-        "-o", "--out", help="Output file.", type=Path, required=True
-    )
-
-    return parser.parse_args()
+from cli import cli
 
 
 def main() -> None:
-    args = get_args()
-
     start_time = time.time()
 
-    if args.command == "count":
-        count_calls_and_parameters(args.src, args.exclude, args.out)
+    cli()
 
     print("\n====================================================================================================")
     print(f"Program ran in {time.time() - start_time}s")
